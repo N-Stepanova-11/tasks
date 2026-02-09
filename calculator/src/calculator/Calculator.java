6fc7e864bd249f7b34988a7645fa86ae6bb158aa
Nadezhda Stepanova <nstepanova063@gmail.com>
2026-02-09 21:33:24 +0300

    refactoring calculator

--- calculator/src/calculator/Calculator.java
+++ calculator/src/calculator/Calculator.java
@@ -1,19 +1,22 @@
 package calculator;
 
 public class Calculator {
-	public static int add(int value1, int value2) {
+	public double add(double value1, double value2) {
 		return value1+value2;
 	}
 	
-	public static int substract(int value1, int value2) {
+	public double subtract(double value1, double value2) {
 		return value1-value2;
 	}
 	
-	public static int multiply(int value1, int value2) {
+	public double multiply(double value1, double value2) {
 		return value1*value2;
 	}
 	
-	public static double divide(int value1, int value2) {
-		return (double)value1/(double)value2;
+	public double divide(double value1, double value2) {
+		if (value2!=0) {
+			return (double)value1/(double)value2;
+		}
+		return 0;
 	}
 }
--- calculator/src/calculator/Program.java
+++ calculator/src/calculator/Program.java
@@ -1,17 +1,23 @@
 package calculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Value1: ");
-		int value1 = scanner.nextInt();
-		System.out.print("Value2: ");
-		int value2 = scanner.nextInt();
-		System.out.println("add: " + Calculator.add(value1, value2));
-		System.out.println("substract: " + Calculator.substract(value1, value2));
-		System.out.println("multiply: " + Calculator.multiply(value1, value2));
-		System.out.println("divide: " + Calculator.divide(value1, value2));
+		try(Scanner scanner = new Scanner(System.in)){
+			System.out.print("Value1: ");
+			double value1 = scanner.nextDouble();
+			System.out.print("Value2: ");
+			double value2 = scanner.nextDouble();
+			
+			Calculator calc = new Calculator();
+			System.out.printf("add: %.2f\n", calc.add(value1, value2));
+			System.out.printf("substract: %.2f\n", calc.subtract(value1, value2));
+			System.out.printf("multiply: %.2f\n", calc.multiply(value1, value2));
+			System.out.printf("divide: %.2f\n", calc.divide(value1, value2));
+		}catch(InputMismatchException e) {
+			System.out.println("Ошибочные входные данные!");
+		}
 	}
 }
