c8da4267ee8b0b9a4985bfc346df96237e31a145
Nadezhda Stepanova <nstepanova063@gmail.com>
2026-02-10 12:04:28 +0300

    add bank menu

--- bank/src/training/bank/Program.java
+++ bank/src/training/bank/Program.java
@@ -4,19 +4,44 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
+	public static void showMenu() {
+		System.out.println("Выберите код операции:");
+		System.out.println("1 - Внести средства на счет");
+		System.out.println("2 - Снять средства со счета ");
+		System.out.println("3 - Показать баланс");
+		System.out.println("4 - Выйти");
+	}
+	
 	public static void main(String[] args) {
 		try(Scanner scanner = new Scanner(System.in)){
 			System.out.print("Номер аккаунта: ");
 			int accountNumber = scanner.nextInt();
 			BankAccount bankAccount = new BankAccount(accountNumber);
 			
-			System.out.print("Внесите средства на счет: ");
-			bankAccount.deposit(scanner.nextDouble());
-
-			System.out.print("Списать со счета: ");
-			bankAccount.withdraw(scanner.nextDouble());
-			
-			System.out.println(bankAccount.getInfo());
+			boolean running = true;
+			while (running) {
+				showMenu();				
+				int menuItem = scanner.nextInt();
+				if (menuItem<1 || menuItem>4) {
+					throw new IllegalArgumentException("Нет такой операции!");
+				}
+				switch (menuItem) {
+					case 1:
+						System.out.print("Внесите средства на счет: ");
+						bankAccount.deposit(scanner.nextDouble());
+						break;
+					case 2:
+						System.out.print("Списать со счета: ");
+						bankAccount.withdraw(scanner.nextDouble());
+						break;
+					case 3:
+						System.out.println(bankAccount.getInfo());
+						break;
+					case 4:
+						running = false;
+						break;
+				}
+			}
 		}catch(InputMismatchException e) {
 			System.out.println("Данные введены неверно!");
 		}catch(IllegalArgumentException e) {
